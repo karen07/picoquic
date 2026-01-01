9e4f9efca14d53fb8b62b46c24c5520812577fc0
Khachatryan Karen <karen0734@gmail.com>
2026-01-01 14:12:20 +0300

    Update for format

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-#build last picotls master (for Travis)
+# build last picotls master (for Travis)
 
 # Build at a known-good commit
-COMMIT_ID= f350eab60742138ac62b42ee444adf04c7898b0d
+COMMIT_ID=f350eab60742138ac62b42ee444adf04c7898b0d
 
-cd ..
+cd .. || exit
 # git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls
 git clone https://github.com/h2o/picotls
-cd picotls
+cd picotls || exit
 git checkout -q "$COMMIT_ID"
 git submodule init
 git submodule update
+# shellcheck disable=SC2154
 echo "Using options:  $PTLS_CMAKE_OPTS"
-cmake $PTLS_CMAKE_OPTS .
-make -j$(nproc) all
-cd ..
+cmake "$PTLS_CMAKE_OPTS" .
+make -j "$(nproc)" all
+cd .. || exit
