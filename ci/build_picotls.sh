23ae3836ede305f60672ef2016f4fd8dcc2fe746
Khachatryan Karen <karen0734@gmail.com>
2026-01-24 12:40:07 +0300

    Format add

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -5,7 +5,8 @@
 COMMIT_ID=f350eab60742138ac62b42ee444adf04c7898b0d
 
 cd .. || exit
-# git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls
+# git clone --branch master --single-branch --shallow-submodules --recurse-submodules \
+#     --no-tags https://github.com/h2o/picotls
 git clone https://github.com/h2o/picotls
 cd picotls || exit
 git checkout -q "$COMMIT_ID"
